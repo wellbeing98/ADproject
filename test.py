ce181562c58e24aa732067c853527fde1babdfe0
JB-point <54920342+JB-point@users.noreply.github.com>
2019-12-18 18:17:29 +0900

    new test

--- test.py
+++ test.py
@@ -6,38 +6,56 @@ from testAD import *
 
 class SeoulMetro(QWidget):
 
+    #용도 모름
+    paintTrigger = pyqtSignal()
+
     def __init__(self, parent= None):
         super().__init__(parent)
+
+        self.dict = {'1': Qt.darkBlue, '2': Qt.green, '3': Qt.darkYellow, '4': Qt.blue, '5': Qt.cyan, '6': Qt.gray,
+                     '7': Qt.darkGray, '8': Qt.red, '9': Qt.gray, '분': Qt.yellow}
+
+        # 없으니까 에러 남
+        self.line1 = QLine()
+        self.line2 = QLine()
+        self.line3 = QLine()
+        self.line4 = QLine()
 
         self.setup_ui()
 
     def setup_ui(self):
 
         # Find
-        #strStart 출발지 입력
+        # 출발안내 라벨
+        self.startLabel = QLabel("출발역을 입력해주세요!")
+
+        # strStart 출발지 입력
         self.strStart = QLineEdit()
         self.strStart.setMaxLength(15)
 
-        #strEnd 도착지 입력
+        # 도착안내 라벨
+        self.endLabel = QLabel("도착역을 입력해주세요!")
+
+        # strEnd 도착지 입력
         self.strEnd = QLineEdit()
         self.strEnd.setMaxLength(15)
 
-        #strStart, strEnd를 입력한 후 버튼 클릭
+        # strStart, strEnd를 입력한 후 버튼 클릭
         self.findBtn = QToolButton()
         self.findBtn.setText("Find!")
         self.findBtn.clicked.connect(self.findEvent)
 
-        #functionBtn
-        self.functionBtn= QToolButton()
-        self.functionBtn.setText("Function!")
-        self.functionBtn.clicked.connect(self.functionEvent)
+        # drawBtn 역 사이 호선을 그림
+        self.drawBtn = QToolButton()
+        self.drawBtn.setText("Function!")
+        self.drawBtn.clicked.connect(self.drawEvent)
 
-        #없으니까 에러 남
-        self.line1= QLine()
-        self.line2 = QLine()
-        self.line3 = QLine()
-        self.line4 = QLine()
+        self.resetBtn = QToolButton()
+        self.resetBtn.setText("reset")
+        self.resetBtn.clicked.connect(self.resetEvent)
 
+        # statusNull 빈공간
+        self.statusNull = QLabel("")
 
         # End
         # timeLabel time표시
@@ -49,48 +67,37 @@ class SeoulMetro(QWidget):
         self.strTime.setAlignment(Qt.AlignLeft)
         self.strTime.setMaxLength(10)
 
-        # statusNull 빈공간
-        self.statusNull= QLabel("")
-
         #statusLayout 오른쪽 레이아웃
         statusLayout = QGridLayout()
-        statusLayout.addWidget(self.strStart, 0, 0, 1, 2)
-        statusLayout.addWidget(self.strEnd, 1, 0, 1, 2)
-        statusLayout.addWidget(self.findBtn, 2, 0)
-        statusLayout.addWidget(self.functionBtn, 2, 1)
+        statusLayout.addWidget(self.startLabel, 0, 0, 1, 3)
+        statusLayout.addWidget(self.strStart, 1, 0, 1, 3)
+        statusLayout.addWidget(self.endLabel, 2, 0, 1, 3)
+        statusLayout.addWidget(self.strEnd, 3, 0, 1, 3)
+        statusLayout.addWidget(self.findBtn, 4, 0)
+        statusLayout.addWidget(self.drawBtn, 4, 1)
+        statusLayout.addWidget(self.resetBtn, 4, 2)
 
-        statusLayout.addWidget(self.statusNull, 3, 0)
+        statusLayout.addWidget(self.statusNull, 5, 0)
 
-        statusLayout.addWidget(self.timeLabel, 4, 0)
-        statusLayout.addWidget(self.strTime, 4, 1)
+        statusLayout.addWidget(self.timeLabel, 6, 0)
+        statusLayout.addWidget(self.strTime, 6, 1, 1, 2)
 
         # show
         # metroLayout 역 구간 보이는 부분
-        self.metroLayout = QVBoxLayout()
-
-        self.nullSpace1 = QHBoxLayout()
-        self.nullSpace2 = QHBoxLayout()
-        self.nullSpace3 = QHBoxLayout()
-        self.nullSpace4 = QHBoxLayout()
-        self.nullSpace5 = QHBoxLayout()
-
-        self.metroLayout.addLayout(self.nullSpace1)
-        self.metroLayout.addLayout(self.nullSpace2)
-        self.metroLayout.addLayout(self.nullSpace3)
-        self.metroLayout.addLayout(self.nullSpace4)
-        self.metroLayout.addLayout(self.nullSpace5)
+        self.metroLayout = QHBoxLayout()
 
         # 시각적으로 나타내는 부분
         # fstBtn 시작역
         # endBtn 도착역
         # spaceNull 빈공간 및 환승역 나타내기용
         self.fstBtn = QToolButton()
-        self.fstBtn.setText("   fst   ")
+        self.fstBtn.setText("fst")
         self.fstBtn.setFixedWidth(150)
 
-        self.endBtn = QToolButton()
-        self.endBtn.setText("   end   ")
-        self.endBtn.setFixedWidth(150)
+        # ==========================================================
+        self.finalBtn = QToolButton()
+        self.finalBtn.setText("final")
+        self.finalBtn.setFixedWidth(150)
 
         self.spaceNull1 = QPushButton("")
         self.spaceNull1.setFixedWidth(15)
@@ -113,7 +120,6 @@ class SeoulMetro(QWidget):
         self.spaceNull7 = QPushButton("")
         self.spaceNull7.setFixedWidth(15)
 
-        # ============
         effect= QGraphicsOpacityEffect(self.spaceNull1)
         effect.setOpacity(0)
         self.spaceNull1.setGraphicsEffect(effect)
@@ -141,22 +147,27 @@ class SeoulMetro(QWidget):
         effect = QGraphicsOpacityEffect(self.spaceNull7)
         effect.setOpacity(0)
         self.spaceNull7.setGraphicsEffect(effect)
-        # =============
+        # =====================================================
 
-        self.nullSpace3.addWidget(self.fstBtn)
+        self.metroLayout.addWidget(self.fstBtn)
 
-        self.nullSpace3.addWidget(self.spaceNull1)
-        self.nullSpace3.addWidget(self.spaceNull2)
-        self.nullSpace3.addWidget(self.spaceNull3)
-        self.nullSpace3.addWidget(self.spaceNull4)
-        self.nullSpace3.addWidget(self.spaceNull5)
-        self.nullSpace3.addWidget(self.spaceNull6)
-        self.nullSpace3.addWidget(self.spaceNull7)
+        self.metroLayout.addWidget(self.spaceNull1)
+        self.metroLayout.addWidget(self.spaceNull2)
+        self.metroLayout.addWidget(self.spaceNull3)
+        self.metroLayout.addWidget(self.spaceNull4)
+        self.metroLayout.addWidget(self.spaceNull5)
+        self.metroLayout.addWidget(self.spaceNull6)
+        self.metroLayout.addWidget(self.spaceNull7)
 
-        self.nullSpace3.addWidget(self.endBtn)
+        self.metroLayout.addWidget(self.finalBtn)
+
+        self.recentStatus = QTextEdit("결과")
+        self.recentStatus.setReadOnly(True)
+        self.recentStatus.setAlignment(Qt.AlignLeft)
 
         showLayout = QGridLayout()
         showLayout.addLayout(self.metroLayout, 0, 0)
+        showLayout.addWidget(self.recentStatus, 1, 0)
 
         # Display Window
         # 왼쪽 레이아웃 = 환승구간 표시
@@ -164,27 +175,29 @@ class SeoulMetro(QWidget):
         mainLayout = QGridLayout()
         mainLayout.setSizeConstraint(QLayout.SetFixedSize)
 
-        mainLayout.addLayout(statusLayout, 0, 1)
         mainLayout.addLayout(showLayout, 0, 0)
+        mainLayout.addLayout(statusLayout, 0, 1)
 
         self.setLayout(mainLayout)
         self.setWindowTitle("Seoul metro find")
+        #=====================
 
     # FindClicked가 눌렸을 때 발생하는 이벤트
     # 입력한 역을 이용해 소요시간과 환승역을 구함
     def findEvent(self):
-        self.startString = self.strStart.text()
-        self.endString = self.strEnd.text()
+        self.fstString = self.strStart.text()
+        self.finalString = self.strEnd.text()
 
-        self.fstBtn.setText(self.startString)
-        self.endBtn.setText(self.endString)
+        self.fstBtn.setText(self.fstString)
+        self.finalBtn.setText(self.finalString)
 
-        self.midlle= Gettime_station_history(self.startString, self.endString)
+        self.midlle = Gettime_station_history(self.fstString, self.finalString)
 
         self.strTime.setText(str(self.midlle.Gettime()))
 
         self.midlleStation= self.midlle.GetTransit_station()
 
+        #환승역이 한 개인 경우
         if len(self.midlleStation) == 1:
             self.spaceNull4.setText(self.midlleStation[0])
             effect = QGraphicsOpacityEffect(self.spaceNull4)
@@ -219,25 +232,32 @@ class SeoulMetro(QWidget):
             self.spaceNull6.setGraphicsEffect(effect)
 
     # 역에서 역 사이 구간의 좌표를 QLine으로 받음
-    def functionEvent(self):
+    def drawEvent(self):
         # 환승역의 개수에 따라 선을 그려야할 구간 설정
         self.midlleLine = self.midlle.GetStation_line()
 
         if len(self.midlleLine) == 1:
+            print("1")
+            self.line1 = QLine(QPoint(self.fstBtn.x() + 10, self.fstBtn.y() + 10),
+                               QPoint(self.finalBtn.x() + 10, self.finalBtn.y() + 10))
+        elif len(self.midlleLine) == 2:
+            print("2")
             self.line1 = QLine(QPoint(self.fstBtn.x()+ 10, self.fstBtn.y()+ 10),
                 QPoint(self.spaceNull4.x()+ 10, self.spaceNull4.y()+ 10))
             self.line2 = QLine(QPoint(self.spaceNull4.x()+ 10, self.spaceNull4.y()+ 10),
-                QPoint(self.endBtn.x()+ 10, self.endBtn.y()+ 10))
+                QPoint(self.finalBtn.x()+ 10, self.finalBtn.y()+ 10))
 
-        elif len(self.midlleLine) == 2:
+        elif len(self.midlleLine) == 3:
+            print("3")
             self.line1 = QLine(QPoint(self.fstBtn.x()+ 10, self.fstBtn.y()+ 10),
                 QPoint(self.spaceNull2.x()+ 10, self.spaceNull2.y()+ 10))
             self.line2 = QLine(QPoint(self.spaceNull2.x()+ 10, self.spaceNull2.y()+ 10),
                 QPoint(self.spaceNull6.x()+ 10, self.spaceNull6.y()+ 10))
             self.line3 = QLine(QPoint(self.spaceNull6.x()+ 10, self.spaceNull6.y()+ 10),
-                QPoint(self.endBtn.x()+ 10, self.endBtn.y()+ 10))
+                QPoint(self.finalBtn.x()+ 10, self.finalBtn.y()+ 10))
 
-        elif len(self.midlleLine) == 3:
+        elif len(self.midlleLine) == 4:
+            print("4")
             self.line1 = QLine(QPoint(self.fstBtn.x()+ 10, self.fstBtn.y()+ 10),
                 QPoint(self.spaceNull2.x()+ 10, self.spaceNull2.y()+ 10))
             self.line2 = QLine(QPoint(self.spaceNull2.x()+ 10, self.spaceNull2.y()+ 10),
@@ -245,31 +265,47 @@ class SeoulMetro(QWidget):
             self.line3 = QLine(QPoint(self.spaceNull4.x()+ 10, self.spaceNull4.y()+ 10),
                 QPoint(self.spaceNull6.x()+ 10, self.spaceNull6.y()+ 10))
             self.line4 = QLine(QPoint(self.spaceNull6.x()+ 10, self.spaceNull6.y()+ 10),
-                QPoint(self.endBtn.x()+ 10, self.endBtn.y()+ 10))
+                QPoint(self.finalBtn.x()+ 10, self.finalBtn.y()+ 10))
 
+        print(self.midlleLine)
         self.update()
 
     # FunctionEvent가 눌렸을 때 QLine에 호선에 맞는 선을 그림
+    '''
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        #painter.begin(self)
+        self.draw_middleLine(painter)
+        #painter.end()
+
+    def draw_middleLine(self, painter):
+        while self.i< len(self.midlleLine):
+            pen = QPen(QColor(self.dict[str(self.midlleLine[self.i])]), 3)
+            painter.setPen(pen)
+            painter.drawLine(self.line1)
+            self.i += 1
+    '''
+
     '''
     def paintEvent(self, event):
 
         if len(self.midlleLine) == 1:
             if not self.line1.isNull():
                 painter1 = QPainter(self)
-                pen1 = QPen(self.selectPen[int(self.midlleLine2[0])], 3)
+                pen1 = QPen(QColor(self.dict[str(self.midlleLine[0])]), 3)
                 painter1.setPen(pen1)
                 painter1.drawLine(self.line1)
 
             if not self.line2.isNull():
                 painter2 = QPainter(self)
-                pen2 = QPen(Qt.red, 3)
+                pen2 = QPen(QColor(self.dict[str(self.midlleLine[1])]), 3)
                 painter2.setPen(pen2)
                 painter2.drawLine(self.line2)
 
         elif len(self.midlleLine) == 2:
             if not self.line1.isNull():
                 painter1 = QPainter(self)
-                pen1 = QPen(self.selectPen[int(self.midlleLine2[0])], 3)
+                pen1 = QPen(QColor(), 3)
                 painter1.setPen(pen1)
                 painter1.drawLine(self.line1)
 
@@ -288,7 +324,7 @@ class SeoulMetro(QWidget):
         elif len(self.midlleLine) == 3:
             if not self.line1.isNull():
                 painter1 = QPainter(self)
-                pen1 = QPen(self.selectPen[int(self.midlleLine2[0])], 3)
+                pen1 = QPen(QColor(), 3)
                 painter1.setPen(pen1)
                 painter1.drawLine(self.line1)
 
@@ -309,7 +345,11 @@ class SeoulMetro(QWidget):
                 pen4 = QPen(Qt.red, 3)
                 painter4.setPen(pen4)
                 painter4.drawLine(self.line4)
-        '''
+    '''
+
+    def resetEvent(self):
+        pass
+
 
 if __name__ == '__main__':
     import sys
